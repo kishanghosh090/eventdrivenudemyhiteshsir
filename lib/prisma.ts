ed5b12efb5d89e2531a073915894a292e81c5870
kishanghosh090 <kishanghosh090@gmail.com>
2026-08-18 23:11:16 +0530

    add client

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -12,3 +12,5 @@ const globalForPrisma = globalThis as unknown as {
 };
 
 export const prisma = globalForPrisma.prisma ?? prismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
