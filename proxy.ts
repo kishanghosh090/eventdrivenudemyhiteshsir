0687d396136835b3a8f9459ca5dd09c65a24ae6f
kishanghosh090 <kishanghosh090@gmail.com>
2026-08-20 23:21:17 +0530

    added

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,48 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import {authMiddleware, clerkClient} from "@clerk/nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export default clerkMiddleware();
+const publicRoutes = [
+  "/",
+  "/api/webhook/register",
+  "sign-in",
+  "sign-up"
+]
+export default authMiddleware({
+  publicRoutes,
+  async afterAuth(auth, req) {
+    if (!auth.userId && !publicRoutes.includes(req.nextUrl.pathname)) {
+      return NextResponse.redirect(new URL("/sign-in",req.url), )
+    }
+    try {
+      if (auth.userId) {
+        const user =await clerkClient.users.getUser(auth.userId)
+        const role = user.publicMetadata.role as string | undefined
+  
+        // admin role redirection
+        if (role === "admin" && req.nextUrl.pathname === "/dashboard") {
+          return NextResponse.redirect(new URL("/admin/dashboard", req.url))
+        }
+  
+        //prevent non admin user to access the admin routes
+  
+        if (role != "admin" && req.nextUrl.pathname.startsWith("/admin")) {
+          return NextResponse.redirect(new URL("/dashboard", req.url))
+        }
+  
+        if (publicRoutes.includes(req.nextUrl.pathname)) {
+          return NextResponse.redirect(new URL(role == "admin" ? "/admin/dashboard" : "/dashboard", req.url))
+        }
+  
+        
+  
+      }
+    } catch (error) {
+      console.error(error);
+      return NextResponse.redirect(new URL("/error",req.url))
+      
+    }
+  }
+})
 
 export const config = {
   matcher: [
